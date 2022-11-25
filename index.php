22d449932e3b500b6c3c5531871d95ccdaf04e6a
Alejandro Garay <agaraym1407@gmail.com>
2022-11-25 03:47:52 -0500

    Version preliminar sin probar bases de datos

--- index.php
+++ index.php
@@ -367,7 +367,7 @@
 							</div>
 
 							<div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
-								<a href="ProductosMujeres.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
+								<a href="ProductosMujer.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
 									Click Aqui
 								</a>
 							</div>
